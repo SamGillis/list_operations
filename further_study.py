9396dabf6dd4a79eb64346d125d27b03fafede51
vagrant <vagrant@vagrant.vm>
2020-10-01 21:19:13 +0000

    Implemented custom_equality function to test if lists are identical

--- further_study.py
+++ further_study.py
@@ -225,8 +225,14 @@ def custom_equality(some_list, another_list):
         False
 
     """
+    
+    if custom_len(some_list) != custom_len(another_list):
+        return False
 
-    return None
+    for i in range(custom_len(some_list)):
+        if some_list[i] != another_list[i]:
+            return False
+    return True
 
 
 # This is the part were we actually run the doctests.
